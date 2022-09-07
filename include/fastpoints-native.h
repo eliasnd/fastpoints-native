99497bc94e4e7f1479bcf50ebb8e573d3f14349a
eliasnd <elias@ndcoding.com>
2022-09-06 21:20:34 -0400

    added decimation

--- include/fastpoints-native.h
+++ include/fastpoints-native.h
@@ -9,13 +9,31 @@
 #include "laslib/inc/las2las.hpp"
 #include "callbackstream.h"
 
+#include "laspoint.hpp"
+#include "lasreader.hpp"
+
 using namespace std;
 using namespace potree_converter;
 
 typedef void (*LoggingCallback)(const char *message);
 
+struct Vector3
+{
+    float x;
+    float y;
+    float z;
+};
+
+struct Color {
+    U16 r;
+    U16 b;
+    U16 g;
+    U16 a;
+};
+
 extern "C" {
     const char* HelloWorld();
     const char* TestCallback(LoggingCallback cb);
     void RunConverter(char* source, char* outDir, LoggingCallback cb);
+    void PopulateDecimatedCloud(char* source, Vector3* points, Color* colors, int decimatedSize, LoggingCallback cb);
 }
--- src/fastpoints-native.cpp
+++ src/fastpoints-native.cpp
@@ -54,4 +54,38 @@ extern "C" {
             remove(file_name);
         }
     }
+
+    DllExport void PopulateDecimatedCloud(char* source, Vector3* points, Color* colors, int decimatedSize, LoggingCallback cb) {
+        cb("start");
+        LASreadOpener lasreadopener;
+        cb("1");
+        lasreadopener.set_file_name(source);
+        cb("2");
+        LASreader* reader = lasreadopener.open();
+        cb("3");
+        int interval = reader->npoints / decimatedSize;
+        cb(("count " + std::to_string(reader->p_count) + " or " + std::to_string(reader->npoints)).c_str());
+        cb("4");
+        int idx = 0;
+        cb("5");
+
+        for (int i = 0; i < decimatedSize; i++) {
+            reader->seek(idx);
+            if (i % 1000 == 0) {
+                cb(("on point " + std::to_string(i)).c_str());
+                cb(("seeking idx " + std::to_string(idx)).c_str());
+                cb(("reader got point " + std::to_string(reader->point.X) + ", " + std::to_string(reader->point.Y) + ", " + std::to_string(reader->point.Z)).c_str());
+                cb(("reader got color " + std::to_string(reader->point.rgb[0]) + ", " + std::to_string(reader->point.rgb[1]) + ", " + std::to_string(reader->point.rgb[2]) + ", " + std::to_string(reader->point.rgb[3])).c_str());
+            }
+            points[i] = (Vector3){reader->point.X, reader->point.Y, reader->point.Z};
+            colors[i] = (Color){reader->point.rgb[0], reader->point.rgb[1], reader->point.rgb[2], reader->point.rgb[3]};
+            idx += interval;
+        }
+
+        cb("6");
+
+        reader->close();
+
+        cb("7");
+    }
 }
